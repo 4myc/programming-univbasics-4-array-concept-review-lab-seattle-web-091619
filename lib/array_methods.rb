c89f590b331bf97871523ae2db9e58c23eb1a22d
Amy C <missch3n@gmail.com>
2019-09-15 20:41:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ def find_element_index(array, value_to_find)
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
-  puts scale[0, 2]
+  puts scale[0]
+  puts scale_2p[1]
   
 end
 
