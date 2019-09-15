142e1246bcf3a7626ec56772bf3a6620d6903dab
Amy C <missch3n@gmail.com>
2019-09-15 20:53:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,9 +6,9 @@ def find_max_value(array)
 
   scale.max
   scale_2.max
-  [1,2,3,4,5,4,3,2,1].max
-  [5,4,3,2,1,0,1,2,3,4,5].max
-  [500,4,1000,5,250].max
+  hill.max
+  valley.max
+  zig_zag.max
 end
 
 def find_min_value(array)
