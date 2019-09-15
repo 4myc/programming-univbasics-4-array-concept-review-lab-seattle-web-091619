7973a42b3d50b1c6bc6499af924034ad78e3879f
Amy C <missch3n@gmail.com>
2019-09-15 20:53:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,12 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-
-  scale.max
-  scale_2.max
-  hill.max
-  valley.max
-  zig_zag.max
+  puts scale.max
+  puts scale_2.max
+  puts hill.max
+  puts valley.max
+  puts zig_zag.max
 end
 
 def find_min_value(array)
