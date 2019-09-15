e8e4f55342f990b62340786790ca98725fa8dc43
Amy C <missch3n@gmail.com>
2019-09-15 20:39:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,9 @@ def find_element_index(array, value_to_find)
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
-  p scale[0,2]
-  p scale_2[0,1]
+  p scale[0]
+  p scale[2]
+  
 end
 
 def find_max_value(array)
