d4de7014285d2e190097db83a9a3db2a1a64ceb1
Amy C <missch3n@gmail.com>
2019-09-15 20:55:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,8 @@ def find_max_value(array)
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
-  [2,3,4,5].find_max_valuep
+  [2,3,4,5].max
+  puts scale
 end
 
 def find_min_value(array)
