df960b1723f39768726d7b3cf97837abe087c5d6
Amy C <missch3n@gmail.com>
2019-09-15 20:58:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,17 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  scale = [2,3,4,5]
-  scale.max
-  scale_2 = [6,5,4,3]
-  scale_2.max
-  hill = [1,2,3,4,5,4,3,2,1]
-  hill.max
-  valley = [5,4,3,2,1,0,1,2,3,4,5]
-  valley.max
-  zig_zag = [500,4,1000,5,250]
-  zig_zag.max
-  
+
   
 end
 
